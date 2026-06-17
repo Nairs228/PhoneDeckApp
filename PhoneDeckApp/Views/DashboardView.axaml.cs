8524819f9e338ac4a9ecfe13cd688947e06d13ef
Nairs228 <sashakim229@gmail.com>
2026-06-17 20:08:11 +0300

    Страница дашборда: карточки станций, поиск, пагинация

---
+++ PhoneDeckApp/Views/DashboardView.axaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+
+namespace PhoneDeckApp.Views
+{
+
+    public partial class DashboardView : UserControl
+    {
+        public DashboardView() => InitializeComponent();
+    }
+}
