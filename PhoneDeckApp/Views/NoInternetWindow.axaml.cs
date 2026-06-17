d2d25a6e4d35c11b00234863a0da2e292e722a13
Nairs228 <sashakim229@gmail.com>
2026-06-17 20:08:01 +0300

    Окно отсутствия интернета: предупреждение при потере соединения

---
+++ PhoneDeckApp/Views/NoInternetWindow.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace PhoneDeckApp.Views;
+
+public partial class NoInternetWindow : Window
+{
+    public NoInternetWindow() => InitializeComponent();
+
+    private void CloseButton_Click(object? sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
